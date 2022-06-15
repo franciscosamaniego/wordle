e3b398a9e9cec8123d523cdfc722194bafdad65d
franciscosamaniego <franciscosamaniego.fs@gmail.com>
2022-06-15 18:00:15 -0300

    prueba

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // palabras para el juego, agregar las que quieran siempre de 5 letras
 // es un array de strings (dict por dictionary)
 let dict = [
@@ -23,3 +24,34 @@ let dict = [
   'genio',
   'pibes'
 ];
+// ocultar perdiste y Ganaste
+document.getElementById('win').style.display = 'none';
+document.getElementById('perdiste').style.display = 'none';
+// intentos
+function button(e) {
+    let bien = 'gatos';
+    let wordle = document.getElementById('game');
+    let res = document.getElementById('res').value;
+    for (let i = 0; i < 1; i++) {
+      let row = document.createElement("div");
+      row.className = "tabla";
+      for(let j = 0; j < 5; j++) {
+        let caja = document.createElement("div");
+        caja.textContent = res[j];
+        if(bien.includes(res[j])) {
+          caja.style.backgroundColor = 'yellow';
+        } else {
+          caja.style.backgroundColor = 'gray';
+        }
+        if(res[j] == bien[j]) {
+          caja.style.backgroundColor = 'green';
+        }
+        caja.className = "cajas";
+        row.appendChild(caja);
+      }
+      wordle.appendChild(row);
+      if(bien == res) {
+        document.getElementById('win').style.display = 'block';
+      }
+    }
+}
